db6dc3dd079a6ce9e2ddac401a52ebe88f19a7e7
fifa1219 <fakhriaja1219@gmail.com>
2022-11-28 12:12:48 +0700

    first commit

---
+++ src/components/CardS.vue
@@ -0,0 +1,15 @@
+<template>
+    <a id="ch2" href="" class="d-flex flex-column p-3 rounded nav-link" style="background-color: white;">
+        <img :src="require('./img/' + cardimage + '.png')" alt="">
+        <div class="pt-3 nav-link text-dark"><b>{{ cardtitle }}</b></div>
+        <div class="nav-link text-dark">{{ cardprice }}</div>
+    </a>
+</template>
+<script>
+export default {
+  name: 'CardS',
+  props: [
+    "cardimage","cardprice","cardtitle"
+  ]
+}
+</script>
